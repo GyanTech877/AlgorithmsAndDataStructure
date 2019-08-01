723eefe2018aa56d063da250ffd0a2f73694f693
Tech Gyan <53473704+GyanTech877@users.noreply.github.com>
2019-08-01 11:28:11 +0530

    Update SlidingWindowMaximum.java

--- slidingwindow/SlidingWindowMaximum.java
+++ slidingwindow/SlidingWindowMaximum.java
@@ -19,7 +19,7 @@ Window position                Max
 Note: 
 You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
 */
-class Solution {
+class Solution1 {
     public int[] maxSlidingWindow(int[] nums, int k) {
         if(nums.length==0 || k<=0) return new int[0];
         int[] result=new int[nums.length-k+1];
@@ -30,6 +30,27 @@ class Solution {
             deque.offer(i);
             if(i-k+1>=0)
             result[i-k+1]=nums[deque.peek()];
+        }
+        return result;
+    }
+}
+
+class Solution2 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums.length==0 || k<=0) return new int[0];
+        int l=nums.length;
+        int[] leftSlidingMax=new int[l];
+        leftSlidingMax[0]=nums[0];
+        int[] rightSlidingMax=new int[l];
+        rightSlidingMax[l-1]=nums[l-1];
+        int[] result=new int[l-k+1];
+        for(int i=1;i<l;i++){
+            leftSlidingMax[i]=(i%k==0)?nums[i]:Math.max(nums[i],leftSlidingMax[i-1]);
+            int j=l-i-1;
+            rightSlidingMax[j]=(j%k==0)?nums[j]:Math.max(nums[j],rightSlidingMax[j+1]);
+        }
+        for(int i=0;i<=l-k;i++){
+            result[i]=Math.max(rightSlidingMax[i],leftSlidingMax[i+k-1]);
         }
         return result;
     }
