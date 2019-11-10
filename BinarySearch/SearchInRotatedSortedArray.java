d81a73d55e127dc00d951ccb60b782149dc066d0
Tech Gyan <53473704+GyanTech877@users.noreply.github.com>
2019-11-10 16:20:48 +0530

    Update SearchInRotatedSortedArray.java

--- BinarySearch/SearchInRotatedSortedArray.java
+++ BinarySearch/SearchInRotatedSortedArray.java
@@ -1,64 +1,80 @@
 public class SearchInRotatedSortedArray {
-	
-	//Without duplicate element
+
+	// Without duplicate element
 	/*
-	 * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
-
-	(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
-
-	You are given a target value to search. If found in the array return its index, otherwise return -1. You may assume no duplicate exists in the array.
-	Your algorithm's runtime complexity must be in the order of O(log n).
+	 * Suppose an array sorted in ascending order is rotated at some pivot unknown
+	 * to you beforehand.
+	 * 
+	 * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+	 * 
+	 * You are given a target value to search. If found in the array return its
+	 * index, otherwise return -1. You may assume no duplicate exists in the array.
+	 * Your algorithm's runtime complexity must be in the order of O(log n).
 	 */
-	
+
 	public int search(int[] nums, int target) {
-		if(nums==null || nums.length==0) return -1;
-		int low=0;
-		int high=nums.length-1;
-		while(low<=high) {
-			int mid=low+(high-low)/2;
-			if(nums[mid]==target) return mid;
-			//Lower half is sorted
-			if(nums[mid]>=nums[low]) {
-				if(target<=nums[mid] && target>=nums[low]) high=mid-1;
-				else low=mid+1;
+		if (nums == null || nums.length == 0)
+			return -1;
+		int low = 0;
+		int high = nums.length - 1;
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (nums[mid] == target)
+				return mid;
+			// Lower half is sorted
+			if (nums[mid] >= nums[low]) {
+				if (target <= nums[mid] && target >= nums[low])
+					high = mid - 1;
+				else
+					low = mid + 1;
 			}
-			//Upper half is sorted
+			// Upper half is sorted
 			else {
-				if(target>=nums[mid] && target<=nums[high]) low=mid+1;
-				else high=mid-1;
+				if (target >= nums[mid] && target <= nums[high])
+					low = mid + 1;
+				else
+					high = mid - 1;
 			}
 		}
 		return -1;
 	}
-	
-	//With duplicate element
-	/*Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
-	(i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
-
-	You are given a target value to search. If found in the array return true, otherwise return false.
+	// With duplicate element
+	/*
+	 * Suppose an array sorted in ascending order is rotated at some pivot unknown
+	 * to you beforehand.
+	 * 
+	 * (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+	 * 
+	 * You are given a target value to search. If found in the array return true,
+	 * otherwise return false.
 	 */
-	
-	 public boolean searchWithDup(int[] nums, int target) {
-	        if(nums==null || nums.length==0) return false;
-			int low=0;
-			int high=nums.length-1;
-			while(low<=high) {
-				int mid=low+(high-low)/2;
-				if(nums[mid]==target) return true;
-				//Lower half is sorted
-				if(nums[mid]>nums[low]) {
-					if(target<nums[mid] && target>=nums[low]) high=mid-1;
-					else low=mid+1;
-				}
-				//Upper half is sorted
-				else if(nums[mid]<nums[low]){
-					if(target>nums[mid] && target<=nums[high]) low=mid+1;
-					else high=mid-1;
-				}
-	            else low++;
-			}
-			return false;
-	    }
 
+	public boolean searchWithDup(int[] nums, int target) {
+		if (nums == null || nums.length == 0)
+			return false;
+		int low = 0;
+		int high = nums.length - 1;
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (nums[mid] == target)
+				return true;
+			// Lower half is sorted
+			if (nums[mid] > nums[low]) {
+				if (target < nums[mid] && target >= nums[low])
+					high = mid - 1;
+				else
+					low = mid + 1;
+			}
+			// Upper half is sorted
+			else if (nums[mid] < nums[low]) {
+				if (target > nums[mid] && target <= nums[high])
+					low = mid + 1;
+				else
+					high = mid - 1;
+			} else
+				low++;
+		}
+		return false;
+	}
 }
